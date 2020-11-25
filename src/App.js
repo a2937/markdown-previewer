6d8c3c8da9a950e17c0b70935ab88c39e69f63b8
Aaron <a.rcottrill521@gmail.com>
2020-11-24 19:15:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import DomPurify from 'dompurify'
+import marked from 'marked'
+import 'marked'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+
+  constructor() {
+
+    super();
+
+    this.state = {
+
+      body: '# h1 \n ## h2 '
+
+    };
+
+
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+
+
+  handleChange(event) {
+
+    this.setState({ body: event.target.value });
+    var rawText = event.target.value;
+    var markdown = marked(rawText).replace(/\r?\n/g, "<br />");
+    document.getElementById("preview").innerHTML = DomPurify.sanitize(markdown);
+  }
+
+
+
+  handleSubmit(event) {
+
+    console.log(this.state);
+
+    event.preventDefault();
+  }
+
+
+
+  render() {
+
+    return (
+
+      <div id="body">
+
+        <h1>Markdown previewer</h1>
+        <p>This app lets you render markdown into html</p>
+        <img src="./notebook.jpg" alt="notebook" height="15%" width="15%" />
+        <br />
+        <strong>Markdown:</strong>
+        <br />
+        <textarea id="editor"
+
+          value={this.state.body}
+
+          onChange={this.handleChange} />
+
+
+        <p id="preview">
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+      </div>
+
+    );
+
+  }
+
 }
+
 
 export default App;
